d8ffcfb528d9b0ae4fb8bbbe05fcbdc923f73864
Briankimp <bkimathi973@gmail.com>
2025-01-03 02:58:40 +0300

    Made a Bunchof changes

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const days = document.querySelectorAll("days");
 const hours = document.querySelectorAll("hours");
 const minutes = document.querySelectorAll("minutes");
 const seconds = document.querySelectorAll("seconds");
+
+const currentYear = new Date().getFullYear();
+const newYearTime = new Date(`January 1 ${currentYear + 1} 00:00:00`);
+
+//Update Countdown timer
+function UpdateCountdownTimer() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+  const d = Math.floor(diff / 1000 / 60);
+
+  console.log(diff);
+}
+UpdateCountdownTimer();
